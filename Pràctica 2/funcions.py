15c2a0a519cd633a068dde3417be9e3b45d422d2
Pablocano2 <08pcano@institutperevives.cat>
2025-01-16 13:24:58 +0100

    git commit

--- Pràctica 2/funcions.py
+++ Pràctica 2/funcions.py
@@ -1,5 +1,9 @@
 def iteracions_MCD_Euclides(a, b):
     i = 0
+    if  b < a:
+        aux= b
+        b = a
+        a= aux
     while b != 0:
         a, b = b, a % b
         i += 1
--- Pràctica 2/main.py
+++ Pràctica 2/main.py
@@ -2,3 +2,9 @@ from funcions import iteracions_MCD_Euclides
 x = []
 y = []
 z = []
+num=10
+for i in range(1,num+1):
+    for j in range(1, num+1):
+        x.append(i)
+        y.append(j)
+        z.append(iteracions_MCD_Euclides)
