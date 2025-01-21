50ea892cca684c240ca3963bf32e6de8a7b787b4
Pablocano2 <08pcano@institutperevives.cat>
2025-01-21 12:48:28 +0100

    git commit

--- Pràctica 2/funcions.py
+++ Pràctica 2/funcions.py
@@ -1,10 +1,13 @@
-def iteracions_MCD_Euclides(a, b):
+
+def iteracions_MCD_Euclides(x, y):
+    a = max(x, y)
+    b = min(x, y)
     i = 0
-    if  b < a:
-        aux= b
-        b = a
-        a= aux
+   
     while b != 0:
-        a, b = b, a % b
         i += 1
-    return i
+        r = a % b
+        a = b
+        b = r
+       
+    return i 
--- Pràctica 2/main.py
+++ Pràctica 2/main.py
@@ -1,10 +1,14 @@
+import matplotlib.pyplot as plt
 from funcions import iteracions_MCD_Euclides
 x = []
 y = []
 z = []
-num=10
+num=250
 for i in range(1,num+1):
     for j in range(1, num+1):
         x.append(i)
         y.append(j)
-        z.append(iteracions_MCD_Euclides)
+        z.append(iteracions_MCD_Euclides(i,j))
+plt.scatter(x, y, c=z)
+plt.colorbar()
+plt.show()
