55a0f6e0b0d70f5377d3abe731018c9507013d01
Pablocano2 <08pcano@institutperevives.cat>
2025-02-05 13:20:18 +0100

    5/2

--- pràctica 4/funcions.py
+++ pràctica 4/funcions.py
@@ -1,6 +1,20 @@
 def f(x):
     return 2*x-4
+
 def compleix_bolzano(f, a, b):
     if f(a) * f(b) < 0:
         return True
     return False
+
+def biseccio(f, a, b, tol):
+    i = 0
+    xm= (a+b)/2
+    while f(xm)>tol:
+        if f(a)*f(xm)<0:
+            b=xm
+        else:
+            a=xm
+
+        i += 1
+    xm= (a+b)/2
+    return xm,f(xm),i 
---
+++ pràctica 4/main.py
@@ -0,0 +1,13 @@
+from funcions import f, compleix_bolzano, biseccio
+
+tol=1e-10
+a=-3
+b= 0
+
+
+
+print(compleix_bolzano(f, a, b))
+print(biseccio(f, a, b, tol))
+
+
+
